b8cd3a1261b833bfeea19efddfff314519392224
Jinhyeok Choi <reality331@khu.ac.kr>
2024-01-18 04:33:20 +0900

    Get file path in a new way
    The method to get the location of xlsx file is changed because the previous code was affacted by working directory.

--- Code/labelchecker.py
+++ Code/labelchecker.py
@@ -2,7 +2,9 @@ from openpyxl import load_workbook
 from pathlib import Path
 
 # xlsx 파일은 Data 폴더에 넣을 것.
-filePath = Path("Data") / input("Enter file name: ")
+# Path 객체에서 상대 경로는 working directory를 기준으로 해석됨.
+# 어디에서 코드를 실행할지 확실치 않으므로 Data 폴더 찾을 때 현재 코드 위치를 기준으로 찾도록 해야 함.
+filePath = Path(__file__).parents[1] / "Data" / input("Enter file name: ")
 wb = load_workbook(filePath, data_only=True)
 ws = wb.active  # active sheet 선택
 
